ace06399f05e3a742e26e709d877561d81a11395
Raghunandan Srinivasan <esraghu@gmail.com>
2021-05-01 22:56:46 +0530

    Fix for displaying correct dates Reading from local storage

--- scripts/main.js
+++ scripts/main.js
@@ -1,82 +1,110 @@
 window.onload = () => {
-  setDate() // set the dates of this week based on todays date
-  readHabits() // read the saved habits from the file
-}
-
-readHabits = () => {
-  fetch('data/data.json')
-    .then(rsp => {
-      return rsp.json()
-    })
-    .then(habits => {
-      const table = document.querySelector('table')
-      for(const habit of habits) {
-        console.log(habit)
-        const row = table.insertRow()
-        row.className = "habit"
-        row.id = habit.habit
-        const hName = row.insertCell(0)
-        const goal = row.insertCell(1)
-        const elements = `<td><input type="checkbox"></td>
-        <td><input type="checkbox"></td>
-        <td><input type="checkbox"></td>
-        <td><input type="checkbox"></td>
-        <td><input type="checkbox"></td>
-        <td><input type="checkbox"></td>
-        <td><input type="checkbox"></td>
-        <td><output>0%</output></td>`
-        row.insertAdjacentHTML('beforeend', elements)        
-        hName.innerHTML = habit.habit
-        goal.innerHTML = `<input type="number" min="1" max="7" id="goal" value=${habit.goal}>`
+  setTheDates() // set the dates of this week based on todays date
+  // readHabits() // read the saved habits from the file
+  // listenForChange()
+  // let habits = localStorage.getItem('habits')
+  if(localStorage.getItem('habits') == null) {
+    let habitls = {
+      1: {
+        "habit" : "Engaged at Work",
+        "goal" : 5
+      },
+      2: {
+        "habit" : "Run or Walk or Workout",
+        "goal" : 4
+      },
+      3: { 
+        "habit" : "Crosswords",
+        "goal" : 4
       }
-      let habitName = document.querySelectorAll(".habit")
-      habitName.forEach(habit => {
-        habit.addEventListener('click', () => {
-          console.log(habit)
-          let target = habit.querySelector('#goal').value || 7
-          let counter = 0
-          habit.querySelectorAll('input[type=checkbox]').forEach(checkbox => {
-            if(checkbox.checked) counter++
-          })
-          habit.querySelector('output').innerHTML = `${Math.ceil((counter/target)*100)}%`
-        })
-      });
-    })
-    .catch(err => {
-      console.log(err)
-    })
+    }
+    localStorage.setItem('habits', JSON.stringify(habitls))
+  }
+  readLocalData(JSON.parse(localStorage.getItem('habits')))
 }
 
-setDate = () => {
+readLocalData = habits => {
+  const table = document.querySelector('table')
+  console.log(habits)
+  for(let i in habits) {
+    const row = table.insertRow()
+    row.className = "habit"
+    row.id = habits[i].habit
+    const hName = row.insertCell(0)
+    const goal = row.insertCell(1)
+    const elements = `<td><input type="checkbox"></td>
+    <td><input type="checkbox"></td>
+    <td><input type="checkbox"></td>
+    <td><input type="checkbox"></td>
+    <td><input type="checkbox"></td>
+    <td><input type="checkbox"></td>
+    <td><input type="checkbox"></td>
+    <td><output>0%</output></td>`
+    row.insertAdjacentHTML('beforeend', elements)        
+    hName.innerHTML = habits[i].habit
+    goal.innerHTML = `<input type="number" min="1" max="7" id="goal" value=${habits[i].goal}>`
+  }
+  let habitName = document.querySelectorAll(".habit")
+  habitName.forEach(habit => {
+    habit.addEventListener('click', () => {
+      console.log(habit)
+      let target = habit.querySelector('#goal').value || 7
+      let counter = 0
+      habit.querySelectorAll('input[type=checkbox]').forEach(checkbox => {
+        if(checkbox.checked) counter++
+      })
+      habit.counter = counter
+      // writeChanges(habits)
+      for(i in habits) {
+        if(habits[i].habit == habit.habit) {
+          habits[i].counter = counter
+          console.log(habits[i])
+          localStorage.setItem('habits', JSON.stringify(habitls))
+        }
+      }
+      habit.querySelector('output').innerHTML = `${Math.ceil((counter/target)*100)}%`
+    })
+  });
+}
+
+setTheDates = () => {
   let today = new Date()
   let dd = today.getDay(), mm = eval(today.getMonth() + 1)
   let days = document.querySelector('.heading')
   days.querySelectorAll('.days').forEach(day => {
+    const dateOfDay = new Date()
     dd = today.getDay()
     switch(day.id) {
       default : break
       case 'Sun' : 
-        dd = eval(today.getDate() - dd)
+        dateOfDay.setDate(today.getDate() - today.getDay())
         break;
       case 'Mon' :
-        dd = eval(today.getDate() - dd + 1)
+        dateOfDay.setDate(today.getDate() - today.getDay() + 1)
         break;
       case 'Tue' :
-        dd = eval(today.getDate() - dd + 2)
+        dateOfDay.setDate(today.getDate() - today.getDay() + 2)
         break;
       case 'Wed' :
-        dd = eval(today.getDate() - dd + 3)
+        dateOfDay.setDate(today.getDate() - today.getDay() + 3)
         break;
       case 'Thu' :
-        dd = eval(today.getDate() - dd + 4)
+        dateOfDay.setDate(today.getDate() - today.getDay() + 4)
         break;
       case 'Fri' :
-        dd = eval(today.getDate() - dd + 5)
+        dateOfDay.setDate(today.getDate() - today.getDay() + 5)
         break;
       case 'Sat' :
-        dd = eval(today.getDate() - dd + 6)
+        dateOfDay.setDate(today.getDate() - today.getDay() + 6)
         break;
     }
-    day.innerHTML = `${dd}/${mm}`
+    day.innerHTML = `${dateOfDay.getDate()}/${dateOfDay.getMonth() + 1}`
+  })
+}
+
+writeChanges = data => {
+  const fs = require('fs')
+  fs.writeFile('data/data.json', data, err => {
+    err? alert(`There was an error writing due to ${err}`): console.log(`No error writing to file`)
   })
 }
